a0b77f0ab5449a64209ff714a179dde8c6b67237
aditysuraj <79074847+aditysuraj@users.noreply.github.com>
2021-02-15 13:28:01 +0530

    Update TBomb.sh

--- TBomb.sh
+++ TBomb.sh
@@ -38,8 +38,8 @@ banner() {
         toilet -f mono12 -F border SpeedX
     fi
     echo -e "\e[1;34m For Any Queries Join Me!!!\e[0m"
-    echo -e "\e[1;32m           Telegram: https://t.me/TBombChat \e[0m"
-    echo -e "\e[4;32m   YouTube: https://www.youtube.com/c/SpeedXTech \e[0m"
+    echo -e "\e[1;32m           Telegram: https://t.me/SURAJ YADAV \e[0m"
+    echo -e "\e[4;32m   YouTube: https://www.youtube.com/c/ADITYASURAJ \e[0m"
     echo " "
 
 }
